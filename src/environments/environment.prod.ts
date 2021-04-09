e7e33ba5c26a146393422cd68344a50624093b93
Kyrill Langhans <kyrill@pickar.studio>
2021-04-09 13:51:34 +0200

    Removed env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   externalCommunication: true,
   version: 'missing',
-  sendbirdAppId: '8D505859-4A9C-4990-A93F-7EF1022B16D7',
-  userA: 'pract@solavieve.com',
-  userA_AccessToken: 'a3b0f3084fa2ea82d5a47fbba0df74fd59283e39',
-  userB: '0f7a4c6d-c9f9-44a5-bf0a-9afb3cbff5d7',
+  sendbirdAppId: 'REPLACE-HERE',
+  userA: 'REPLACE-HERE',
+  userA_AccessToken: 'REPLACE-HERE',
+  userB: 'REPLACE-HERE' // no access token for userB
 };
